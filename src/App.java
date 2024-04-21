9ef3d40833221be94b15c827a451e99744018aca
Stefan Dunikovskyi <>
2024-04-21 12:20:10 +0300

    Lab1

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to the standard output.
+ */
+public class App {
+    /**
+     * The main method which serves as the entry point for the application.
+     * 
+     * @param args An array of command-line arguments for the application
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
